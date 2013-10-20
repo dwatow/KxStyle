d847009e8377c4cc53ce10f1affc58c7aca33e4c
kx_wang <dwatow@gmail.com>
2013-10-20 21:26:38 +0800

    add case&if of demo case file

---
+++ demo/testCaseIf.cpp
@@ -0,0 +1,21 @@
+//if 1
+if (a<30)
+{}
+//if 2
+if (a<30){    donothing();  }
+else{    fuckstyle();   }
+//if 3
+if (a<30){
+    donothing();
+}else{
+    fuckstyle();
+}
+//if 4
+if (a<30)
+{
+    donothing();  
+}
+else
+{
+    fuckstyle();   
+}
